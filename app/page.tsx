1228d3da3005ba44c9f33fd52b9facca33d7df7f
walldriyan <walldriyan@gmail.com>
2025-11-03 12:30:22 +0530

    redx ui ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Example from "./components/Example";
 
 export default function Home() {
   return (
+    <>
     <div>home page</div>
+   <Example />
+    </>
+    
   );
 }

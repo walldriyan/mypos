ee87b13a838882c9399593e2e05e3f0b9bba1763
walldriyan <walldriyan@gmail.com>
2025-11-08 11:51:19 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-
-
-import { LSidebar } from "./components/LSidebar";
-import { bottomMenuData, menuData, workspaceData } from "./data/sidebarData";
-
-
 export default function Home() {
-
-
   return (
     <>
- <LSidebar
-            menuItems={menuData}
-            workspaces={workspaceData}
-            bottomItems={bottomMenuData}
-            userInitials="AB"
-          />
+      <div>hi</div>
     </>
   );
 }
